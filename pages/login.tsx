9e663b6f545ba246deea014cdda463768daf6e8d
drinkmonster <berenyibence98@gmail.com>
2023-06-14 19:27:52 +0200

    szia

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet, Pressable, Button, Text } from 'react-native';
+import { View, StyleSheet, Text, Pressable, TextInput } from 'react-native';
 
 let loginPage = () => {
     const styles = StyleSheet.create({
@@ -6,28 +6,89 @@ let loginPage = () => {
             flex: 1,
             display: 'flex',
             flexDirection: 'column',
-            backgroundColor: '#000000',
+            justifyContent: 'space-between',
+            backgroundColor: '#1E1E1E',
+            height: '100vh',
+            width: '100vw',
         },
         line: {
-            width: '100%',
+            width: '100vh',
             height: 1,
-            backgroundColor: '#ffffff',
-            marginVertical: 20,
+            alignSelf: 'center',
+            backgroundColor: '#474747',
         },
         title: {
-            width: '100%',
+            color: '#FFFFFF',
+            fontSize: 40,
             textAlign: 'center',
-            fontSize: 20,
-            color: '#ffffff',
             marginVertical: 20,
+            fontWeight: 'bold',
         },
+        input: {
+            height: 50,
+            borderRadius: 10,
+            borderWidth: 1,
+            borderColor: '#ccc',
+            paddingHorizontal: 10,
+            marginBottom: 10,
+            color: '#FFFFFF',
+            marginHorizontal: 20,
+        },
+        footer: {
+            marginTop: 20,
+            color: '#FFFFFF',
+            textAlign: 'center',
+            fontSize: 14,
+            marginBottom: 20,
+        },
+        loginButton: {
+            height: 50,
+            borderRadius: 10,
+            backgroundColor: '#474747',
+            width: 100,
+            alignSelf: 'center',
+            justifyContent: 'center',
+            alignItems: 'center',
+          },
+          loginButtonText: {
+            color: '#FFFFFF',
+            textAlign: 'center',
+          },          
     });
 
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Login</Text>
+            <View>
+                <Text style={styles.title}>
+                    Login
+                </Text>
+                <View style={styles.line} />
+            </View>
+            <View>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Email Address"
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder="Password"
+                    secureTextEntry
+                />
+                <Pressable style={styles.loginButton}>
+                    <Text style={styles.loginButtonText}>
+                        Login
+                    </Text>
+                </Pressable>
+            </View>
+            <View>
             <View style={styles.line} />
-        </View >
+                <Text style={styles.footer}>
+                    Made by PetrikDevOps
+                </Text>
+            </View>
+        </View>
     );
 };
 
