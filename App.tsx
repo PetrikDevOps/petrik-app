ed20c08d92423593dc8037b330c9a6483eb1091b
Preisler <preisler.andras@gmail.com>
2023-06-19 17:00:36 +0200

    routing

--- App.tsx
+++ App.tsx
@@ -3,6 +3,8 @@ import newPage from './pages/new';
 import helyettesitesPage from './pages/helyettesites';
 import classSelectionPage from './pages/classSelection';
 
+
+
 export default function App() {
   return (
     <div>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,57 +1,70 @@
 import React from 'react';
 import { StyleSheet, View, Text, Pressable } from 'react-native';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import News from '../pages/news';
+import New from '../pages/new';
+import Helyettesites from '../pages/helyettesites';
+import ClassSelection from '../pages/classSelection';
 
 const Navbar = () => {
   return (
-    <View style={styles.navbar}>
+    <Router>
+      <View style={styles.navbar}>
         <Pressable style={styles.navbarActiveButton}>
-            <Text style={styles.navbarButtonText}>
-                H
-            </Text>
+          <Text style={styles.navbarButtonText}>H</Text>
         </Pressable>
         <Pressable style={styles.navbarButton}>
-            <Text style={styles.navbarButtonText}>
-                H
-            </Text>
+          <Text style={styles.navbarButtonText}>H</Text>
         </Pressable>
         <Pressable style={styles.navbarButton}>
-            <Text style={styles.navbarButtonText}>
-                B
-            </Text>
+          <Text style={styles.navbarButtonText}>B</Text>
         </Pressable>
-    </View>
+      </View>
+
+      <Routes>
+        <Route path="/" element={<News />} />
+        <Route path="/ht" element={<Helyettesites />} />
+        <Route path="/cs" element={<ClassSelection />} />
+      </Routes>
+    </Router>
   );
 };
 
 const styles = StyleSheet.create({
-    navbar: {
-        height: 100,
-        backgroundColor: '#252525',
-        boxShadow: '0px -3px 4px #2AB4AB',
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-evenly',
-        paddingTop: 10,
+  navbar: {
+    height: 100,
+    backgroundColor: '#252525',
+    shadowColor: '#2AB4AB',
+    shadowOffset: {
+      width: 0,
+      height: -3,
     },
-    navbarActiveButton: {
-        width: '75px',
-        height: '75px',
-        backgroundColor: '#2AB4AB',
-        borderRadius: 100,
-    },
-    navbarButton: {
-        width: '75px',
-        height: '75px',
-        backgroundColor: '#D9D9D9',
-        borderRadius: 100,
-    },
-    navbarButtonText: {
-        fontSize: 20,
-        color: '#000000',
-        fontWeight: '400',
-        textAlign: 'center',
-        lineHeight: 75,
-    },
+    shadowOpacity: 0.4,
+    shadowRadius: 4,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    paddingTop: 10,
+  },
+  navbarActiveButton: {
+    width: 75,
+    height: 75,
+    backgroundColor: '#2AB4AB',
+    borderRadius: 100,
+  },
+  navbarButton: {
+    width: 75,
+    height: 75,
+    backgroundColor: '#D9D9D9',
+    borderRadius: 100,
+  },
+  navbarButtonText: {
+    fontSize: 20,
+    color: '#000000',
+    fontWeight: '400',
+    textAlign: 'center',
+    lineHeight: 75,
+  },
 });
 
 export default Navbar;
